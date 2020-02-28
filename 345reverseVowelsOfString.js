4bddbf55855bc9f5494cac62411d18c2de03cb0f
David Hsiao <dhsiao510@gmail.com>
2020-02-27 23:53:28 -0800

    Wrong answer. Failed case with Upper Cased vowels.

--- 345reverseVowelsOfString.js
+++ 345reverseVowelsOfString.js
@@ -3,14 +3,14 @@ var reverseVowels = function(s) {
     let arr = s.split('');
     let i = 0;
     let j = s.length - 1;
-    while(i <= j) {
-        if((v.indexOf(arr[i]) > 0) && (v.indexOf(arr[j]) > 0)) {
+    while(i < j) {
+        if((v.indexOf(arr[i]) >= 0) && (v.indexOf(arr[j]) >= 0)) {
             let temp = arr[i];
             arr[i] = arr[j];
             arr[j] = temp;
             i++;
             j--;
-        } else if((v.indexOf(arr[i]) < 0) && (v.indexOf(arr[j]) > 0)){
+        } else if((v.indexOf(arr[i]) < 0) && (v.indexOf(arr[j]) >= 0)){
             i++;
         } else {
             j--;
