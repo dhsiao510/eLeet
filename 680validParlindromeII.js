cf632282b4cc63caee95584bb2e92e2f14bf1d9f
David Hsiao <dhsiao510@gmail.com>
2020-03-07 22:26:12 -0800

    Wrong answer. Failed for input "hbakab"

--- 680validParlindromeII.js
+++ 680validParlindromeII.js
@@ -1,21 +1,30 @@
 var validPalindrome = function(s) {
-    let i = 0; j = s.length -1; deleted = false; result = true;
-    while(i !== j) {
+    let i = 0; j = s.length -1; deleted = false; 
+    while(i < j) {
         if(s.charAt(i) === s.charAt(j)) {
             i++;
             j--;
         } else if ((s.charAt(i)=== s.charAt(j - 1)) && !deleted) {
             deleted = true;
-            j = j - 2;
-            i++;
+            if((j - 2) > i++) {
+                j = j - 2;
+                 i++;  
+            } else {
+                break;
+            }
+
         } else if ((s.charAt(i + 1) === s.charAt(j)) && !deleted) {
             deleted = true;
-            i = i + 2;
-            j--;
+            if((i + 2) < j--) {
+              i = i + 2;
+              j--;
+            } else {
+                break;
+            }
+
         } else {
-            result = false;
-            break;
+            return false;
         }
     }
-    return result; 
+    return true; 
 };
