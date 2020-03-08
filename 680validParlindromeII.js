3ea90f51e84cabb462ce52dbba21909f0152eae9
David Hsiao <dhsiao510@gmail.com>
2020-03-07 23:01:03 -0800

    Wrong answer for input "aguokepatgbnvfqmgmlcupuufxoohdfpgjdmysgvhmvffcnqxjjxqncffvmhvgsymdjgpfdhooxfuupuculmgmqfvnbgtapekouga"

--- 680validParlindromeII.js
+++ 680validParlindromeII.js
@@ -2,27 +2,29 @@ var validPalindrome = function(s) {
     let i = 0; j = s.length -1; deleted = false; 
     while(i < j) {
         if(s.charAt(i) === s.charAt(j)) {
+          if(i + 1 < j- 1){
             i++;
             j--;
-        } else if ((s.charAt(i)=== s.charAt(j - 1)) && !deleted) {
-            deleted = true;
-            if((j - 2) > i++) {
-                j = j - 2;
-                 i++;  
-            } else {
-                break;
-            }
-
+          } else{
+            break;
+          }
         } else if ((s.charAt(i + 1) === s.charAt(j)) && !deleted) {
             deleted = true;
-            if((i + 2) < j--) {
+            if((i + 2) < j - 1) {
               i = i + 2;
               j--;
             } else {
                 break;
             }
-
-        } else {
+        } else if ((s.charAt(i)=== s.charAt(j - 1)) && !deleted) {
+            deleted = true;
+            if((j - 2) > i + 1) {
+                j = j - 2;
+                i++;  
+            } else {
+                break;
+            }
+        }  else {
             return false;
         }
     }
