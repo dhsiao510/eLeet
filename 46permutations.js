ddb6cf528ff3815f2f9d6558b1f31c0a9e458e1c
David Hsiao <dhsiao510@gmail.com>
2020-04-19 17:48:51 -0700

    Completed with internal recursive function

---
+++ 46permutations.js
@@ -0,0 +1,22 @@
+var permute = function(nums) {
+    let result = [];
+    
+    const findPerm = (curr,leftover) => {
+        curr = curr || [];
+        
+        if(!leftover.length) {
+            result.push(curr);
+        }
+        
+        for(let i = 0; i < leftover.length; i++) {
+            const temp = leftover[i];
+            findPerm(curr.concat(temp), leftover.slice(0, i).concat(leftover.slice(i + 1)));
+        }
+    }
+    
+    findPerm([], nums);
+    return result;
+};
+
+//Runtime: 64 ms, faster than 85.68% of JavaScript online submissions for Permutations.
+//Memory Usage: 36.9 MB, less than 24.00% of JavaScript online submissions for Permutations.
