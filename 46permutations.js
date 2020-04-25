97119ed32f392478ef45c6b1440c57a230fd8d83
David Hsiao <dhsiao510@gmail.com>
2020-04-24 17:53:31 -0700

    Review

--- 46permutations.js
+++ 46permutations.js
@@ -1,21 +1,17 @@
 var permute = function(nums) {
-    let result = [];
-    
-    const findPerm = (curr,leftover) => {
-        curr = curr || [];
-        
-        if(!leftover.length) {
-            result.push(curr);
+    let results = [];
+    const permute = (current, leftover) => {
+        if(current.length === nums.length) {
+            results.push(current);
         }
-        
         for(let i = 0; i < leftover.length; i++) {
-            findPerm(curr.concat(leftover[i]), leftover.slice(0, i).concat(leftover.slice(i + 1)));
+            permute(current.concat(leftover[i]), leftover.slice(0, i).concat(leftover.slice(i + 1)))
         }
     }
+    permute([], nums)
     
-    findPerm([], nums);
-    return result;
-};
+    return results; 
+}
 
 //Runtime: 64 ms, faster than 85.68% of JavaScript online submissions for Permutations.
 //Memory Usage: 36.9 MB, less than 24.00% of JavaScript online submissions for Permutations.
