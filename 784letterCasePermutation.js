5b9ada943f30e716863888a2b0089a96dbf35d8b
David Hsiao <dhsiao510@gmail.com>
2020-04-26 15:10:43 -0700

    Review. Used concat instead.

--- 784letterCasePermutation.js
+++ 784letterCasePermutation.js
@@ -1,21 +1,23 @@
 var letterCasePermutation = function(S) {
     let result = [];
     
-    const permutation = (current, i) => {
+    const permute = (current, i) => {
         if(current.length === S.length) {
             result.push(current);
             return;
         }
         
         if(S.charAt(i) >= '0' && S.charAt(i) <= '9') {
-            permutation(current += S.charAt(i), i + 1);
-        } else{
-            permutation(current + S.charAt(i).toUpperCase(), i + 1);
-            permutation(current + S.charAt(i).toLowerCase(), i + 1);
+            permute(current.concat(S.charAt(i)), i + 1);
+        } else {
+            permute(current.concat(S.charAt(i).toUpperCase()), i + 1);
+            permute(current.concat(S.charAt(i).toLowerCase()), i + 1)
         }
     }
     
-    permutation("", 0);
-    
+    permute('', 0)
     return result;
 };
+
+//Runtime: 64 ms, faster than 95.55% of JavaScript online submissions for Letter Case Permutation.
+//Memory Usage: 38.2 MB, less than 33.33% of JavaScript online submissions for Letter Case Permutation.
