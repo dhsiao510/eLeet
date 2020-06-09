0f4e0107d120e80acdacc8f5db8d71831b552327
David Hsiao <dhsiao510@gmail.com>
2020-06-09 15:48:16 -0700

    Completed

---
+++ recursion-capitalizeWords.js
@@ -0,0 +1,8 @@
+function capitalizeWords (arr) {
+    if(arr.length === 1) {
+        return [arr[0].toUpperCase()];
+    }
+    let result = capitalizeWords(arr.slice(0, -1));
+    result.push(arr.slice(arr.length - 1)[0].toUpperCase());
+    return result;
+}
