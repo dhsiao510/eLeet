f4a29da23243a0f6775867638503c5cb7cb7a682
David Hsiao <dhsiao510@gmail.com>
2020-06-30 22:10:47 -0700

    Completed by fixing the conditional

--- 325maxSubArrayLen.js
+++ 325maxSubArrayLen.js
@@ -1,22 +1,23 @@
 var maxSubArrayLen = function(nums, k) {
-    let map = {0: -1};
+    let map = {};
+    map[0] = -1;
     let result = 0;
+    let sum = 0;
     
-    for(let i = 0, sum = 0; i < nums.length; i++) {
+    for(let i = 0; i < nums.length; i++) {
         sum += nums[i];
         
-        if(map[sum - k]) {
-            result = Math.max(result , i- map[sum - k])
+        if(!(sum in map)) {
+            map[sum] = i
         }
         
-        if(!map[sum]) {
-            map[sum] = i
+        if(sum - k in map) {
+            result = Math.max(result , i- map[sum - k])
         }
     }
     
     return result;
 };
 
-//[-2,-1,2,1], 1
-//Output 1
-//Expected 2
+//Runtime: 100 ms, faster than 46.99% of JavaScript online submissions for Maximum Size Subarray Sum Equals k.
+//Memory Usage: 44.9 MB, less than 10.89% of JavaScript online submissions for Maximum Size Subarray Sum Equals k.
