4f5b6290cd34816a7ed8be922887c2d667acfbd5
David Hsiao <dhsiao510@gmail.com>
2020-12-16 22:02:29 -0800

    Init with push and pop

---
+++ singlyLinkedList.js
@@ -0,0 +1,50 @@
+class Node{
+    constructor(val){
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList{
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0
+    }
+
+    push(val) {
+        let newNode = new Node(val)
+        if(!this.head) {
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+
+    pop() {
+        if(!this.head) {
+            return undefined;
+        }
+        let current = this.head;
+        while(current.next){
+            let prev = current;
+            current = current.next;
+        }
+        this.tail = prev;
+        this.tail.next = null;
+        this.length--;
+        if(this.length === 0) {
+            this.head = null;
+            this.tail = null;
+        }
+        return current;
+    }
+
+    shift() {
+        if(!this.head) return undefined;
+        
+    }
+} 
